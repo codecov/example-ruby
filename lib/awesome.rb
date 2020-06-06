4dec05ad74d88f7c39ba76168f28ffe8523efd4a
Thomas Hu <tomhu1096@gmail.com>
2020-06-06 17:31:58 -0400

    Rubocop

--- lib/awesome.rb
+++ lib/awesome.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 class Awesome
   def initialize
     @initted = 1
   end
+
   def smile
-    return ':)'
+    ':)'
   end
+
   def frown
-    return ':('
+    ':('
   end
 end
--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'simplecov'
 SimpleCov.start 'rails' do
-  add_filter "/test/"
+  add_filter '/test/'
 end
 if ENV['CI'] == 'true'
   require 'codecov'
