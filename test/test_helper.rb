170f0942233f707c5d35e159e1c2d61ced19b500
Tom Hu <tom@codecov.io>
2022-07-26 23:07:09 -0700

    cleanup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require "simplecov"
-
 SimpleCov.start
+
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
 require "calculator"
 require "minitest/autorun"
